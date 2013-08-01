6512f4161cbdf9935095044012507b359776237f
Stian Hanger <pdnagilum@gmail.com>
2013-08-01 13:46:14 +0200

    Add function addEndpoint()

--- OAuth2.php
+++ OAuth2.php
@@ -1,5 +1,28 @@
 <?php
 
+/**
+ * Wrapper framework for OAuth2 with included providers.
+ *
+ * Released under the MIT license.
+ *
+ * Copyright (c) 2013 Stian Hanger
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 namespace OAuth2;
 
 define('ACCESS_TOKEN',          'access_token');
@@ -98,6 +121,15 @@ class Client {
       return $this->public[$name];
 
     return FALSE;
+  }
+
+  public function addEndpoint($providerShortHandName, $urlAuth, $urlToken, $urlUser = NULL, $urlUserIncludeHeader = FALSE) {
+    $this->public[$providerShortHandName] = array(
+      OAUTH_URL_AUTH  => $urlAuth,
+      OAUTH_URL_TOKEN => $urlToken,
+      OAUTH_URL_USER  => $urlUser,
+      OAUTH_URL_USER_HEADER => $urlUserIncludeHeader,
+      );
   }
 
   public function authorize($scope = NULL) {
